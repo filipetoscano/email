317ff8867e4b1b98e9d4b869856bb3abf9d9eb66
Filipe Toscano <filipe.toscano@gmail.com>
2026-01-04 17:45:24 +0300

    Validate port number

--- src/Lefty.Email/Senders/MailkitSenderOptionsValidation.cs
+++ src/Lefty.Email/Senders/MailkitSenderOptionsValidation.cs
@@ -14,6 +14,12 @@ public class MailkitSenderOptionsValidation : IValidateOptions<MailkitSenderOpti
         if ( options.Username != null && options.Password == null )
             return ValidateOptionsResult.Fail( "env:SMTP_PASSWORD is required when username is specified" );
 
+        if ( options.Port < 1 )
+            return ValidateOptionsResult.Fail( "env:SMTP_PORT must be a positive number" );
+
+        if ( options.Port >= 65535 )
+            return ValidateOptionsResult.Fail( "env:SMTP_PORT must be less than 65535" );
+
         return ValidateOptionsResult.Success;
     }
 }
